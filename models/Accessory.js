c1419dc9508325a132892c10555e6f2e901bf5a8
abhishek8094 <iasabhi41@gmail.com>
2025-10-08 08:15:14 +0530

    completed the accessiors

---
+++ models/Accessory.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add an accessory name'],
+    trim: true,
+    maxlength: [100, 'Accessory name cannot be more than 100 characters']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please add a price'],
+    min: [0, 'Price cannot be negative']
+  },
+  image: {
+    type: String,
+    required: [true, 'Please add an image']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Accessory', accessorySchema);
